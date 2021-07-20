1cd3dadfb8feaae3bc68785bbe4cd5bff566d239
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-07-20 17:51:34 -0300

    Fix: ListDivider

--- src/components/ListHeader/index.tsx
+++ src/components/ListHeader/index.tsx
@@ -7,12 +7,14 @@ type Props = { title: string; subtitle: string };
 
 export function ListHeader({ title, subtitle }: Props) {
   return (
-    <>
       <View style={styles.container}>
-        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.title}>
+          { title }
+        </Text>
 
-        <Text style={styles.subtitle}>{subtitle}</Text>
+        <Text style={styles.subtitle}>
+          { subtitle }
+        </Text>
       </View>
-    </>
   );
 }
