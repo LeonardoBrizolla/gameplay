3f75fe6264247a2bb156d69b7289a7e52556c106
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-07-29 23:07:53 -0300

    Aula 03 - In Orbit

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -12,11 +12,13 @@ type Props = RectButtonProps & {
 export function ButtonIcon({ title, ...rest }: Props) {
   return (
     <RectButton style={styles.container} {...rest}>
+
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
+
     </RectButton>
   );
 }
