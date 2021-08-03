42cd99de697cb4f8029f726832dccc81a0f118a2
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-08-03 13:35:32 -0300

    Aula 04 - Landing

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = "https://auth.expo.io/@anonymous/gameplay-05489a6c-3b43-4488-92b5-02bd00c8f9cf";
+const SCOPE = "guilds%20connections%20email%20identify";
+const RESPONSE_TYPE = "token";
+const CLIENT_ID = "870822919687786496";
+const CDN_IMAGE = "https://cdn.discordapp.com";
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE,
+}
